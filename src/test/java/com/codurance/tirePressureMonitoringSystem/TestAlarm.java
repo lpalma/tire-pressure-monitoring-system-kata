cfb85c92a55b87d5d6d1583179b356890dd7a6ed
Luciano Palma <lucianopalma90@gmail.com>
2017-07-19 15:04:27 +0100

    overloard constructor

--- src/test/java/com/codurance/tirePressureMonitoringSystem/TestAlarm.java
+++ src/test/java/com/codurance/tirePressureMonitoringSystem/TestAlarm.java
@@ -1,14 +1,36 @@
 package com.codurance.tirePressureMonitoringSystem;
 
 
-import org.junit.*;
-import static org.junit.Assert.*;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
 
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertThat;
+import static org.mockito.BDDMockito.given;
+
+@RunWith(MockitoJUnitRunner.class)
 public class TestAlarm {
 
+    @Mock
+    private Sensor sensor;
+
     @Test
-    public void foo() {
+    public void have_alarm_set_off_by_default() {
         Alarm alarm = new Alarm();
         assertEquals(false, alarm.isAlarmOn());
+    }
+
+    @Test
+    public void set_on_when_pressure_is_below_threshold() {
+
+        Alarm alarm = new Alarm(sensor);
+
+        given(sensor.popNextPressurePsiValue()).willReturn(16d);
+
+        alarm.check();
+        assertThat(alarm.isAlarmOn(), equalTo(true));
     }
 }
